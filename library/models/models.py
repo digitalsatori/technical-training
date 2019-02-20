012ebb0273be920b9a7af7f270506af2d60544d2
digitalsatori <tony@openerp.cn>
2019-02-20 06:06:27 +0000

    added model for book

---
+++ library/models/models.py
@@ -0,0 +1,10 @@
+from odoo import models, fields
+
+class Books(models.Model):
+    
+    _name = 'library.books'
+    
+    name = fields.Char(string="Name")
+    ISBN = fields.Char(String="ISBN")
+    publish_date = fields.Date(string="Publish Date")
+    book_type = fields.Selection(selection=[('hard', 'Hard Cover'), ('soft', 'Soft Cover')], string="Book Type")
